9448a879b0c0db497729f45da6592fba85b4900a
Mohammad Hassan Farhadi <74290354+mhfarhadi@users.noreply.github.com>
2022-05-17 21:45:12 +0430

    hesam's changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Products from "./components/functional/products" //for functional enable this import line
-// import Products from "./components/products"  //for class based componet active this line
+// import Products from "./components/functional/products" //for functional enable this import line
+import Products from "./components/products"  //for class based componet active this line
 
 export default function App() {
   return (
